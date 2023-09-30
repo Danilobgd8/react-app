527969f9c5b7c36c85adbb9a99721e75cba36008
danilobgd8 <danilo@comit.co.rs>
2023-09-30 13:07:17 +0200

    final commit

--- src/components/dataList.js
+++ src/components/dataList.js
@@ -113,99 +113,98 @@ function DataList() {
         ))}
       </div>
 
-      {currentSmallItems.length > 0 && (
-        <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2">
-          {currentSmallItems.map((item) => (
-            <div
-              key={item.id}
-              className="p-3 bg-white border border-blue-500 transition-transform duration-300 ease-in-out transform hover:scale-165 hover:shadow-lg hover:-translate-y-2 hover:-translate-x-2"
-              style={{
-                width: "540px",
-                height: "217px",
-                border: "1px solid #348ABF",
-                display: "flex",
-                flexDirection: "row",
-                alignItems: "center",
-              }}
-            >
-              <div className="p-3">
-                <h2
+      <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2">
+        {currentSmallItems.map((item) => (
+          <div
+            key={item.id}
+            className="p-3 bg-white border border-blue-500 transition-transform duration-300 ease-in-out transform hover:scale-165 hover:shadow-lg hover:-translate-y-2 hover:-translate-x-2"
+            style={{
+              width: "540px",
+              height: "217px",
+              border: "1px solid #348ABF",
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+            }}
+          >
+            <div className="p-3">
+              <h2
+                style={{
+                  color: "#042440",
+                  fontFamily: "Inter",
+                  fontSize: "10px",
+                  fontStyle: "normal",
+                  fontWeight: 700,
+                  lineHeight: "normal",
+                  textTransform: "uppercase",
+                  marginBottom: "15px",
+                }}
+              >
+                {item.title}
+              </h2>
+              <p
+                style={{
+                  color: " #000",
+                  fontFamily: "Playfair Display",
+                  fontSize: "16px",
+                  fontStyle: "normal",
+                  fontWeight: 500,
+                  lineHeight: "normal",
+                  marginBottom: "10px",
+                }}
+              >
+                {item.description.substring(0, 80)}
+              </p>
+              <p
+                style={{
+                  color: "#808495",
+                  fontFamily: "Inter",
+                  fontSize: "14px",
+                  fontStyle: "normal",
+                  fontWeight: 400,
+                  lineHeight: "normal",
+                  marginBottom: "7px",
+                }}
+              >
+                {item.description}
+              </p>
+              <div>
+                <a
+                  href="/"
                   style={{
-                    color: "#042440",
+                    color: "#348ABF",
                     fontFamily: "Inter",
                     fontSize: "10px",
                     fontStyle: "normal",
                     fontWeight: 700,
-                    lineHeight: "normal",
-                    textTransform: "uppercase",
-                    marginBottom: "15px",
-                  }}
-                >
-                  {item.title}
-                </h2>
-                <p
-                  style={{
-                    color: " #000",
-                    fontFamily: "Playfair Display",
-                    fontSize: "16px",
-                    fontStyle: "normal",
-                    fontWeight: 500,
-                    lineHeight: "normal",
+                    lineHeight: "uppercase",
                     marginBottom: "10px",
                   }}
                 >
-                  {item.description.substring(0, 80)}
-                </p>
-                <p
-                  style={{
-                    color: "#808495",
-                    fontFamily: "Inter",
-                    fontSize: "14px",
-                    fontStyle: "normal",
-                    fontWeight: 400,
-                    lineHeight: "normal",
-                    marginBottom: "7px",
-                  }}
+                  {item.title} <FontAwesomeIcon icon={faArrowRightLong} />
+                </a>
+                <button
+                  onClick={() => deleteItem(item.id, "smallData")}
+                  className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded text-xs ml-auto"
                 >
-                  {item.description}
-                </p>
-                <div>
-                  <a
-                    href="/"
-                    style={{
-                      color: "#348ABF",
-                      fontFamily: "Inter",
-                      fontSize: "10px",
-                      fontStyle: "normal",
-                      fontWeight: 700,
-                      lineHeight: "uppercase",
-                      marginBottom: "10px",
-                    }}
-                  >
-                    {item.title} <FontAwesomeIcon icon={faArrowRightLong} />
-                  </a>
-                  <button
-                    onClick={() => deleteItem(item.id, "smallData")}
-                    className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded text-xs ml-auto"
-                  >
-                    X
-                  </button>
-                </div>
+                  X
+                </button>
               </div>
-              <img
-                src={item.image}
-                alt={item.title}
-                style={{
-                  width: "230px",
-                  height: "197px",
-                  marginLeft: "10px",
-                  marginRight: "10px",
-                }}
-              />
             </div>
-          ))}
-        </div>
-      )}
+            <img
+              src={item.image}
+              alt={item.title}
+              style={{
+                width: "230px",
+                height: "197px",
+                marginLeft: "10px",
+                marginRight: "10px",
+              }}
+            />
+          </div>
+        ))}
+      </div>
+
       <div className="text-center">
         <p>Footer text</p>
       </div>
